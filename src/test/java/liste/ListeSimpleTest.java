5f1f473b66a337cc0b57f2e03acdd4cda7596aa7
Martin <martin.geris@alumni.univ-avignon.fr>
2025-09-18 16:22:12 +0200

    Exercice3-CorrectionAjoutTest

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -63,6 +63,7 @@ class ListeSimpleTest {
         assertEquals(4, listeATester.tete.getSuivant().getElement());
     }
 
+    @Test
     void modifiePremierInexistant() {
         listeATester.ajout(1);
         listeATester.ajout(2);
